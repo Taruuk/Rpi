db4c873ff87343cabda41e67315fba83e797c7ef
Tyson <tysondenobrega@gmail.com>
2020-08-26 23:18:02 -0400

    Update CNCjs_Install.sh

--- CNCjs_Install.sh
+++ CNCjs_Install.sh
@@ -41,3 +41,20 @@ pm2 save
 
 # Get list of PM2 processes
 pm2 list
+
+# Iptables (allow access to port 8000 from port 80)
+sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8000
+
+# Make Iptables Persistent
+sudo apt-get install iptables-persistent -y
+
+# How-to: Save & Reload Rules
+#sudo netfilter-persistent save
+#sudo netfilter-persistent reload
+
+# How-to: Manually Save Rules
+#sudo sh -c "iptables-save > /etc/iptables/rules.v4"
+#sudo sh -c "ip6tables-save > /etc/iptables/rules.v6"
+
+# Run this if issues to reconfigure iptables-persistent
+# sudo dpkg-reconfigure iptables-persistent
